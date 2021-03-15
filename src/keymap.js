21af7101353e86173a2f968481bfda3417e0af27
Urs Hofer <u.hofer>
2021-03-15 15:43:51 +0100

    automatic build

--- src/keymap.js
+++ src/keymap.js
@@ -18,18 +18,8 @@ const splitToDefaultListItem = function(itemType, nodes) {
 
     if (grandParent.type.name == 'description_list' && dispatch) {
 
-      //dispatch(state.tr.replaceSelectionWith(grandParent.type.createAndFill()).scrollIntoView())
-
-      let wrap = Fragment.empty; const keepItem = $from.index(-1) > 0
-      // Build a fragment containing empty versions of the structure
-      // from the outer list item to the parent node of the cursor
-      for (let d = $from.depth - (keepItem ? 1 : 2); d >= $from.depth - 3; d--) wrap = Fragment.from($from.node(d).copy(wrap))
-      // Add a second list item with an empty default start node
-      wrap = wrap.append(Fragment.from(itemType.createAndFill()))
-      const tr = state.tr.replace($from.before(keepItem ? null : -1), $from.after(-3), new Slice(wrap, keepItem ? 3 : 2, 2))
-      tr.setSelection(state.selection.constructor.near(tr.doc.resolve($from.pos + (keepItem ? 3 : 2))))
-      dispatch(tr.scrollIntoView())
-
+      dispatch(state.tr.replaceSelectionWith(grandParent.type.createAndFill()).scrollIntoView())
+      joinUp();
       return true
 
     }
