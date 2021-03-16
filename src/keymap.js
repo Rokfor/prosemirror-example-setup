fd5391abe0a773573bc3fda6663e1a4765ab0e79
Urs Hofer <u.hofer>
2021-03-16 16:50:32 +0100

    automatic build

--- src/keymap.js
+++ src/keymap.js
@@ -34,7 +34,7 @@ function splitDefinitionList(itemType, nodes) {
         let tr = state.tr
         //let range = $from.blockRange($to)
         //let $start = tr.doc.resolve(range.start), item = $start.nodeAfter
-        grandParent.node(-1).content.append(Fragment.from(nodes.paragraph));
+        $from.node(-2).content.append(Fragment.from(nodes.paragraph));
 
         /*
 
