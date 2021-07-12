d1e94ed3caeb9767f5c839246432629d24ed04ad
Urs Hofer <u.hofer>
2021-07-12 15:53:42 +0200

    automatic build

--- src/menu.js
+++ src/menu.js
@@ -213,6 +213,7 @@ function bibliographyItem(markType) {
         callback(attrs) {
           //const mr = view.state.schema.mark(markType, attrs)
           const tr = view.state.tr.replaceSelectionWith(view.state.schema.text(`${attrs.reference} ${attrs.pre} ${attrs.post}`), true) 
+          view.dispatch(tr)
           toggleMark(markType, attrs)(view.state, view.dispatch)
           view.focus()
         }
