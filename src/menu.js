0a03fc5db40503a93c1fd0a0bde305bb543bf7c6
Urs Hofer <u.hofer>
2021-07-12 15:51:29 +0200

    automatic build

--- src/menu.js
+++ src/menu.js
@@ -212,16 +212,8 @@ function bibliographyItem(markType) {
         },
         callback(attrs) {
           //const mr = view.state.schema.mark(markType, attrs)
-
-
           const tr = view.state.tr.replaceSelectionWith(view.state.schema.text(`${attrs.reference} ${attrs.pre} ${attrs.post}`), true) 
-          tr.addStoredMark(view.state.schema.mark(markType, attrs))
-          
-          
-          
-          
-          view.dispatch(tr)
-
+          toggleMark(markType, attrs)(view.state, view.dispatch)
           view.focus()
         }
       })
